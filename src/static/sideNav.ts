2b828edc4f248d21e069d873fef14a00600bed5c
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-06-30 17:28:58 +0545

    feat: static data for the sidenav items and sidebar width

---
+++ src/static/sideNav.ts
@@ -0,0 +1,21 @@
+// Defines the width of the sidebar/navigation drawer
+export const SIDEBAR_WIDTH = 212;
+
+// Defines an array of sidebar navigation items
+export const sidenavItems = [
+  {
+    id: 1,
+    label: 'Datasets',
+    link: '/'
+  },
+  {
+    id: 2,
+    label: 'Workflows',
+    link: '/workflows'
+  },
+  {
+    id: 3,
+    label: 'Credit Usage',
+    link: '/credit-usage'
+  }
+];
