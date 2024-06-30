b4d4aafc80028b02e2a09fbf323c23a9c9036055
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-06-30 20:27:09 +0545

    feat: add main wrapper around the outlet

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,11 +1,14 @@
 import { useCallback, useState } from 'react';
-import { Stack } from '@mui/material';
+import { Box, Stack, useTheme } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../common/Navbar';
 import Sidebar from '../common/Sidebar';
+import { SIDEBAR_WIDTH } from '../../static/sideNav';
 
 const DashboardLayout = () => {
+  const theme = useTheme();
+
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = useCallback(() => {
@@ -16,7 +19,21 @@ const DashboardLayout = () => {
     <Stack direction={'row'}>
       <Sidebar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
       <Navbar handleDrawerToggle={handleDrawerToggle} />
-      <Outlet />
+      <Box
+        component={'main'}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: {
+            sm: `calc(100% - ${SIDEBAR_WIDTH}px)`,
+            backgroundColor: theme.palette.neutral.dark,
+            marginTop: '73px',
+            minHeight: `calc(100dvh - 73px)`
+          }
+        }}
+      >
+        <Outlet />
+      </Box>
     </Stack>
   );
 };
