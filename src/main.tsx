3a6d96655947046773541d8f2472e1cb85094533
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-07-02 22:16:01 +0545

    feat: setting up axios interceptor and query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
 import theme from './utils/theme/index.ts';
+import { setupInterceptorsToInspection } from './services/api.ts';
 import './index.css';
+
+const queryClient = new QueryClient();
+export const Axios = setupInterceptorsToInspection(axios.create());
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
