6c353769265f442af266c6b4a29eae05a03d7e39
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-07-02 22:19:40 +0545

    feat: add page for the dataset

--- src/pages/datasets.tsx
+++ src/pages/datasets.tsx
@@ -1,5 +1,38 @@
+import { Stack } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+
+import DatasetHeader from '../components/container/datasets/DatasetHeader';
+import CustomTable from '../components/common/CustomTable';
+import { Column } from '../type/table';
+import { fetchProducts } from '../services/product';
+
 const Datasets = () => {
-  return <div>Datasets</div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ['products'],
+    queryFn: fetchProducts
+  });
+
+  const columns: Column[] = [
+    { id: 'name', label: 'Product Name', minWidth: 170 },
+    { id: 'price', label: 'Price', minWidth: 100 },
+    { id: 'rating', label: 'Rating', minWidth: 170, align: 'left' },
+    { id: 'brand', label: 'Brand', minWidth: 170, align: 'left' },
+    { id: 'availability', label: 'Availability', minWidth: 170, align: 'left' }
+  ];
+
+  const transformedData =
+    data?.map((item) => ({
+      ...item,
+      price: `$${item.price}`,
+      availability: item.availability ? 'Yes' : 'No'
+    })) || [];
+
+  return (
+    <Stack gap={'1rem'}>
+      <DatasetHeader />
+      <CustomTable columns={columns} data={transformedData} isLoading={isLoading} />
+    </Stack>
+  );
 };
 
 export default Datasets;
